efae7fbf2ef357c1c9feea142e5357be03a23a97
Muhammad Abdul Halim <abdul.halim@bs-23.net>
2021-02-07 09:05:27 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:productivity_timer/timer.dart';
+import 'package:productivity_timer/timer_model.dart';
+import 'package:productivity_timer/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Productivity Timer',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TimerHomePage(),
+    );
+  }
+}
+
+class TimerHomePage extends StatelessWidget {
+  final CountDownTimer timer = CountDownTimer();
+  final double defaultPadding = 5.0;
+  @override
+  Widget build(BuildContext context) {
+    timer.startWork();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Work Time'),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final double availableWidth = constraints.maxWidth;
+          return Column(
+            children: [
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                      child: ProductivityButton(color: Color(0xFF009688), text: 'Work', onPressed: () {}, size: null)),
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                      child: ProductivityButton(
+                          color: Color(0xFF607D88), text: 'Short Break', onPressed: () {}, size: null)),
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                      child: ProductivityButton(
+                          color: Color(0xFF455A64), text: 'Long Break', onPressed: () {}, size: null)),
+                ],
+              ),
+              StreamBuilder(
+                  initialData: "00:00",
+                  stream: timer.stream(),
+                  builder: (context, snapshot) {
+                    TimerModel timer =
+                        (snapshot.data == "00:00") ? TimerModel(time: "00:00", percent: 1) : snapshot.data;
+                    return Expanded(
+                      child: CircularPercentIndicator(
+                        radius: availableWidth / 2,
+                        percent: timer.percent,
+                        progressColor: Color(0xFF009546),
+                        lineWidth: 10.0,
+                        center: Text(
+                          timer.time,
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                      ),
+                    );
+                  }),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                      child: ProductivityButton(color: Color(0xFF009688), text: 'Stop', onPressed: () {}, size: null)),
+                  Padding(padding: EdgeInsets.all(defaultPadding)),
+                  Expanded(
+                      child:
+                          ProductivityButton(color: Color(0xFF607D88), text: 'Restart', onPressed: () {}, size: null)),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/timer.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:productivity_timer/timer_model.dart';
+
+class CountDownTimer {
+  double _radius = 1;
+  bool _isActive = true;
+  Timer timer;
+  Duration _time;
+  Duration _fullTime;
+
+  int workTime = 30;
+
+  void startWork() {
+    _radius = 1;
+    _time = Duration(minutes: this.workTime, seconds: 0);
+    _fullTime = _time;
+  }
+
+  String getFormattedTime(Duration t) {
+    String minutes = (t.inMinutes < 10) ? '0' + t.inMinutes.toString() : t.inMinutes.toString();
+    int numSeconds = t.inSeconds - (t.inMinutes * 60);
+    String seconds = (numSeconds < 10) ? '0' + numSeconds.toString() : numSeconds.toString();
+    return minutes + ":" + seconds;
+  }
+
+  Stream<TimerModel> stream() async* {
+    yield* Stream.periodic(Duration(seconds: 1), (int a) {
+      String time;
+      if (this._isActive) {
+        _time = _time - Duration(seconds: 1);
+        _radius = _time.inSeconds / _fullTime.inSeconds;
+        if (_time.inSeconds <= 0) {
+          _isActive = false;
+        }
+      }
+      time = getFormattedTime(_time);
+      return TimerModel(time: time, percent: _radius);
+    });
+  }
+}
